bc68981a1c6582dd3bc8af54b53031ae3223409a
Donald Trump <fsociety@us.gov>
2018-06-27 15:16:51 -0700

    Replaced processEvent recursion by a for loop. No more recursion depth exceeded error.

--- lines.py
+++ lines.py
@@ -9,21 +9,14 @@ from utility import *
 import sys, resource
 
 
-
 def iterate(y, counter):
     if counter is 0: return y
     return iterate( y+[normal(y[-1],1) ], counter-1 )
 
-def sim(inputs):
-    return iterate( [0], len(inputs)-1 )
-        
-def process(x,y,counter=0):    
-    if counter is 0: return y
-    return process(x,y+sim(x),counter-1)
-
 def init(plates, events):
     x=[i%plates for i in range(events*plates)]
-    y=process(range(plates),[],counter=events)
+    y=[]
+    for i in range(events): y+=iterate( [0], plates-1 )
     return x,y
 
 
